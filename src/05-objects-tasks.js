1c130b9296b940bd11700a527debc2864b83d06c
wenttosmoke <107771767+wenttosmoke@users.noreply.github.com>
2023-01-02 01:43:59 +0300

    update: fix

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -23,7 +23,7 @@
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
+  this.getArea = function name() {
     return this.width * this.height;
   };
 }
